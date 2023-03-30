7359a6eb4c00a0529d929ce4b49431885e5682e1
xortaa <kercwinpaul@gmail.com>
2023-03-30 15:48:42 +0800

    added propmpts for google or mozilla

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const shell = require("shelljs")
 const cron = require("node-cron")
 require("dotenv").config()
+
 const clc = require("cli-color")
 const yellow = clc.yellow
 const green = clc.green
+const yellowBright = clc.yellowBright
+const red = clc.red
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
 
 const openChromeProfileAndLink = async (link) => {
   await shell.exec('start chrome --profile-directory="Profile 5"')
@@ -11,7 +19,35 @@ const openChromeProfileAndLink = async (link) => {
   console.log(yellow(`${link} HAS OPENED`))
 }
 
+const openFirefoxAndLink = (link) => {
+  shell.exec(`start firefox ${link}`)
+  console.log(yellow(`${link} HAS OPENED`))
+}
+
 console.log(green("App is now running"))
+
+
+const prompt = (query) => new Promise((resolve) => readline.question(query, resolve))
+async function getInputs() {
+  try {
+    const browser = await prompt(yellowBright("Which browser are will you be using? (firefox or chrome only): "))
+    if (browser.toLowerCase() === "chrome" || browser.toLowerCase() === "firefox") {
+      console.log(browser)
+    }
+    if (browser.toLowerCase() === "chrome") {
+      const profile = await prompt(yellowBright("Which profile number will you be using? (ex. input: 5): "))
+      console.log(`Using ${browser} --profile-directory="Profile ${profile}`)
+    } else {
+      console.log(red(`Invalid browser ${browser}. firefox or chrome only`))
+      getInputs()
+    }
+
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+getInputs()
 
 cron.schedule("0 9 * * 1", () => {
   openChromeProfileAndLink(process.env.IT2623)
